e7f50209e2a7d7587f404d037d6a87bae9922693
chaojie-fu <fuchaojiemail@gmail.com>
2019-09-04 12:32:17 +0800

    array slice test

---
+++ Day01-15/practice/Day07/array slice'.py
@@ -0,0 +1,15 @@
+"""
+测试数组的切片功能
+切片后的数组是指向旧的数据，还是创建一个新的数据
+"""
+origin = [1, 2, 3, 4, 5]
+print('origin = ', origin)
+slice = origin[1:3]
+print('slice = ', slice)
+origin[1] = 9
+print('origin = ', origin)
+print('slice = ', slice)
+
+"""
+结果表明python的数据切片操作时创建一个新的数组
+"""
